9fce8e3c686fe12f967c85341602e790ed148c01
Harsha-9177 <vaddiharsha8@gmail.com>
2023-08-04 21:15:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { BrowserRouter as Router,   Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+// import About from './pages/About';
+import Products from './components/products';
+import SingleProduct from './pages/SingleProduct';
+import Cart from './pages/Cart';
+import Navigation from './components/navigation';
+import { CartContext } from './pages/CartContext';
+import { useEffect, useState } from 'react';
+// import {useEffect} from  'react';
+
+
+
+
+const App = () =>{
+const [cart ,setCart ]= useState({});
+
+// fetch cart from local storage
+
+
+// useEffect(() => {
+//   const cart = window.localStorage.getItem('cart');
+//   setCart(JSON.parse(cart));
+
+// }, [])
+
+// useEffect(() => {
+//     const cart = window.localStorage.getItem('cart',JSON.stringify(cart));
+  
+// }, [cart])
+
+
+    return (
+        <>
+            <Router>
+                <CartContext.Provider value={{ cart, setCart }} >
+
+                  <Navigation />
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    {/* <Route path="/About" component={About}></Route> */}
+                    <Route path="/products"  element={<Products />}/>
+                    <Route path="/products/:_id"  element={<SingleProduct/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+
+                </Routes>
+                </CartContext.Provider>
+            </Router>
+        </>
+    )
+}
+export default App;
---
+++ src/helper.js
@@ -0,0 +1,12 @@
+import pizzaData from './components/PizzaData.json';
+
+
+
+export const getAllPizzas = ()=>{
+    return pizzaData.pizzaData;
+}
+
+export const getPizzaById = (id)=>{
+    console.log(id,"inside helper");
+    return pizzaData.pizzaData.find((pizza)=>pizza._id+''===id);
+}
